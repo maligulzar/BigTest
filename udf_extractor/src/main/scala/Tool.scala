0e506c4b45cad229e434cab157957b7544a3503a
Muhammad Ali Gulzar <gulzarmuhammadali@gmail.com>
2017-08-07 14:33:47 -0700

    UDF Type Extractor

--- udf_extractor/src/main/scala/Tool.scala
+++ udf_extractor/src/main/scala/Tool.scala
@@ -6,12 +6,17 @@ object  Tool {
 
 
   def main (args: Array[String] ) {
+
+    WordCount.main(Array[String]())
+///Users/malig/workspace/git/Test-Minimization-in-Big-Data/udf_extractor/src/main/scala/WordCount.scala
     val conf = new Configuration("conf.txt").loadMapping().enableSparkMutation()
-    val inputdir = "work/input/SparkWordCount.scala"
+    val inputdir = "src/main/scala/WordCount.scala"
     val pathtosrc = "work/input/"   // usually /src/main
-    val outputdir = "work/output/SparkWordCount.scala"
+    val outputdir = "work/output/SparkWordCount.txt"
     val ex = new Extractor()
 //    ex.run(conf,inputdir,pathtosrc, outputdir)
     ex.runOneFile(conf, inputdir, outputdir)
+
   }
+
 }
