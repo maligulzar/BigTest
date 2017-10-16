bd73e729edd8fa340f2c4612a6ff35989f4623ee
ShaghayeghMrdn <shaghayeghmardani@gmail.com>
2017-10-16 14:57:04 -0700

    Added another test which calls udf_extractor to get the _+_ udf

---
+++ ScalaSymbolicExecution/src/main/scala/SymexScala/SymbolicRDD.scala
@@ -0,0 +1,29 @@
+package SymexScala
+
+class SymbolicRDD(/*ofType: VType, */name: String, es: Array[SymVar]) {
+    //val actualType = RDD<ofType>
+    val elements: Array[SymVar] = es
+
+    def aggregateOnRDD(effect: NonTerminal): NonTerminal = {
+        //SymVar, Expr -> Expr
+        val l = 
+            if(effect.left.isInstanceOf[SymVar]) effect.left.applyEffect(effect.left.asInstanceOf[SymVar], elements(0))  
+            else null
+
+        val r = 
+            if(effect.right.isInstanceOf[SymVar]) effect.right.applyEffect(effect.right.asInstanceOf[SymVar], elements(1))  
+            else null
+
+        // val root = new NonTerminal(l, effect.middle, r)
+        
+        updateTree(effect, elements.head, elements.tail).asInstanceOf[NonTerminal]
+    }
+
+    def updateTree(effect:NonTerminal, root: Expr, rdd: Array[SymVar]): Expr = {
+        if(rdd.size > 0) {
+            val newRoot = new NonTerminal(root, effect.middle, rdd.head)
+            updateTree(effect, newRoot, rdd.tail)    
+        }
+        else root
+    }
+}
--- udf_extractor/src/main/scala/Test7.scala
+++ udf_extractor/src/main/scala/Test7.scala
@@ -1,19 +1,18 @@
 import org.apache.spark.{ SparkContext, SparkConf }
 
-object Test5 {
+object Test7 {
 
     def main(args: Array[String]): Unit = {
 
         val conf = new SparkConf()
-                    .setAppName("Scala Toy Example 5: Join preparation")
+                    .setAppName("Scala Toy Example 1: Add Integers")
                     .setMaster("local[4]")
         val sc = new SparkContext(conf)
         val sum = sc.textFile("input")
-                    .map(line => Integer.parseInt(line))
-                    .map(x => (x, 1))
-                    .map(x => if(x._1 > 100) x._1 else 0)
-                    
-                    //.reduce(_+_)
+                    .flatMap(line => line.split(" "))
+                    .map(number => Integer.parseInt(number))
+                    // .map(x => if(x > 100) x else 0)
+                    .reduce(_+_)
 
         // println("Sum: "+sum)
 
